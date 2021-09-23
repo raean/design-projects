e4245283544de06060450904794135544a3b42aa
Rae Abunahla <raghebabn@gmail.com>
2021-09-23 09:46:01 -0400

    gh-pages fix

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -20,11 +20,11 @@ export function NaviBar(props) {
                 </a>
                 </div>
                 <ul class="menu">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/#about">About</a></li>
                 <li><a href="/design-projects"> Design </a></li>
                 <li><a href="/code-projects"> Code </a></li>
-                <li><a href="#contact">Contact</a></li>
+                {/* <li><a href="#contact">Contact</a></li> */}
                 <li><a onclick="window.open('./docs/Resume-Rae-Abunahla.pdf','_blank')" class="resume">Resume</a></li>
                 </ul>
                 <div className="menu-btn">
