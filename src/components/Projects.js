742a5de03977edf18e61a0e12094058fdc9f8735
Rae Abunahla <raghebabn@gmail.com>
2021-09-26 23:56:51 -0400

    merge fix with code projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,17 +12,15 @@ export function Projects(props) {
         <div class="about" id="about">
             <div class="max-width">
                <h2 class="title">Coding Projects </h2>
-                {/* <button class={"project-button" + (selectedProject === 0 ? "-selected" : "")} onClick={() => setSelectedProject(0)}> All In </button>
+                <button class={"project-button" + (selectedProject === 0 ? "-selected" : "")} onClick={() => setSelectedProject(0)}> All In </button>
                 <button class={"project-button" + (selectedProject === 1 ? "-selected" : "")} onClick={() => setSelectedProject(1)}> Junior Workshop Designer</button>
                 <button class={"project-button" + (selectedProject === 2 ? "-selected" : "")} onClick={() => setSelectedProject(2)}> Sustainable Housing Development </button>
                 <button class={"project-button" + (selectedProject === 3 ? "-selected" : "")} onClick={() => setSelectedProject(3)}> SmartTrax </button> */}
-                {/* {selectedProject === 0 && <div><AllIn/></div>}
+                {selectedProject === 0 && <div><AllIn/></div>}
                 {selectedProject === 1 && <div><Hub/></div>}
                 {selectedProject === 2 && <div>Proj2</div>}
-                {selectedProject === 3 && <div><SmartTrax/></div>} */}
-                <div style={{padding: "0"}}  class="about content">
-                    <i>Coming soon!</i>
-                </div>
+                {selectedProject === 3 && <div><SmartTrax/></div>}
+                
             </div>
         </div>
     )
