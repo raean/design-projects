814372904f06faecdc9036226894b211ae65141e
Rae Abunahla <raghebabn@gmail.com>
2021-09-26 23:45:52 -0400

    coming soon messaging

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,5 @@
 import React, {useState} from 'react';
 import '../index.css';
-import tcfLogo from '../images/tcf-logo.png'
-import tcfPage from '../images/tcf-page.png'
-import Accordion from 'react-bootstrap/Accordion'
-import { AllIn } from './AllIn';
-import { Hub } from './Hub';
-import { SmartTrax } from './SmartTrax';
 
 /**
  * Renders the Nominations components which provides a list of the nominated movies found, their title, their year released, their poster as well as a button to remove the movie from their nominations list.
@@ -17,15 +11,18 @@ export function Projects(props) {
     return (
         <div class="about" id="about">
             <div class="max-width">
-               <h2 class="title">Design Projects </h2>
-                <button class={"project-button" + (selectedProject === 0 ? "-selected" : "")} onClick={() => setSelectedProject(0)}> All In </button>
+               <h2 class="title">Coding Projects </h2>
+                {/* <button class={"project-button" + (selectedProject === 0 ? "-selected" : "")} onClick={() => setSelectedProject(0)}> All In </button>
                 <button class={"project-button" + (selectedProject === 1 ? "-selected" : "")} onClick={() => setSelectedProject(1)}> Junior Workshop Designer</button>
                 <button class={"project-button" + (selectedProject === 2 ? "-selected" : "")} onClick={() => setSelectedProject(2)}> Sustainable Housing Development </button>
-                <button class={"project-button" + (selectedProject === 3 ? "-selected" : "")} onClick={() => setSelectedProject(3)}> SmartTrax </button>
-                {selectedProject === 0 && <div><AllIn/></div>}
+                <button class={"project-button" + (selectedProject === 3 ? "-selected" : "")} onClick={() => setSelectedProject(3)}> SmartTrax </button> */}
+                {/* {selectedProject === 0 && <div><AllIn/></div>}
                 {selectedProject === 1 && <div><Hub/></div>}
                 {selectedProject === 2 && <div>Proj2</div>}
-                {selectedProject === 3 && <div><SmartTrax/></div>}
+                {selectedProject === 3 && <div><SmartTrax/></div>} */}
+                <div style={{padding: "0"}}  class="about content">
+                    <i>Coming soon!</i>
+                </div>
             </div>
         </div>
     )
