1a5c565c8764c3823d9d360fc7e879b5ab115ef4
Rae Abunahla <raghebabn@gmail.com>
2021-09-26 23:54:36 -0400

    view fix

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -25,7 +25,7 @@ export function NaviBar(props) {
                 <li><a href="/design-projects"> Design </a></li>
                 <li><a href="/code-projects"> Code </a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a onclick="window.open('./docs/Resume-Rae-Abunahla.pdf','_blank')" class="resume">Resume</a></li>
+                <li><a onclick="window.open('./docs/rae-one-pager.pdf','_blank')" class="resume">Resume</a></li>
                 </ul>
                 <div className="menu-btn">
                 <i class="fas fa-bars"></i>
